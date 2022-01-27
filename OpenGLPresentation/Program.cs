1e7403063ded8cd24f87883ea8ed7a0b731e067e
Niki123456123456 <niklasnordkamp@web.de>
2022-01-27 12:18:21 +0100

    initial commit

---
+++ OpenGLPresentation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OpenGLPresentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleExtension.Hide();
+            using (Window window = new Window())
+            {
+                window.Run();
+            }
+        }
+    }
+}
---
+++ OpenGLPresentation/VertexArray.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL4;
+
+namespace OpenGLPresentation
+{
+    public delegate void DescribeVertexArray();
+    public class VertexArray
+    {
+        public int Id { get; }
+        public VertexArray(float[] vertices, DescribeVertexArray describeVertexArray)
+        {
+            // VertexArray erstellen
+            Id = GL.GenVertexArray();
+            GL.BindVertexArray(Id);
+
+            // Zwischenspeicher erstellen
+            int bufferId = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, bufferId);
+
+            // Zwischenspeicher füllen
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+
+            // Zwischenspeicher beschreiben
+            describeVertexArray?.Invoke();
+            
+            // unbind
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.BindVertexArray(0);
+
+        }
+    }
+}
